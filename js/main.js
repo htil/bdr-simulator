cb9cf5d3e1f4b47e914976d8b443ab90a87ee12a
BryanHernandez04 <byhernandez@crimson.ua.edu>
2019-04-11 11:22:09 -0500

    New BCIDevice build + other beautiful stuff.

--- js/main.js
+++ js/main.js
@@ -108,46 +108,99 @@ window.onload = () => {
 		engagement: -1
 	};
 
-	window.Device = new Bluetooth.BCIDevice((sample) => {
-		if (Bluetooth.BCIDevice.electrodeIndex("AF7") !== sample.electrode) return;
+	// window.Device = new Bluetooth.BCIDevice((sample) => {
+	// 	if (Bluetooth.BCIDevice.electrodeIndex("AF7") !== sample.electrode) return;
 
-		sample.data.forEach(el => {
-			if (buffer.length > BUFFER_SIZE) buffer.shift();
-			buffer.push(el);
-		});
+	// 	sample.data.forEach(el => {
+	// 		if (buffer.length > BUFFER_SIZE) buffer.shift();
+	// 		buffer.push(el);
+	// 	});
 
-		if (buffer.length < BUFFER_SIZE) return;
+	// 	if (buffer.length < BUFFER_SIZE) return;
 
-		let psd = window.bci.signal.getPSD(BUFFER_SIZE, buffer);
+	// 	let psd = window.bci.signal.getPSD(BUFFER_SIZE, buffer);
 
-		let alpha = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "alpha");
-		let beta = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "beta");
-		let theta = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "theta");
-		let engagement = beta / (alpha + theta);
-		let sum = alpha + beta + theta;
+	// 	let alpha = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "alpha");
+	// 	let beta = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "beta");
+	// 	let theta = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "theta");
+	// 	let engagement = beta / (alpha + theta);
+	// 	let sum = alpha + beta + theta;
 
-		let w_alpha = alpha / sum;
-		let w_beta = beta / sum;
-		let w_theta = theta / sum;
+	// 	let w_alpha = alpha / sum;
+	// 	let w_beta = beta / sum;
+	// 	let w_theta = theta / sum;
 
-		if (weighted.alpha < 0) {
-			weighted.alpha = w_alpha || 0;
-			weighted.beta = w_beta || 0;
-			weighted.theta = w_theta || 0;
-			weighted.engagement = engagement || 0;
-		} else {
-			weighted.alpha = weighted.alpha * WEIGHT + (w_alpha || 0) * (1 - WEIGHT);
-			weighted.beta = weighted.beta * WEIGHT + (w_beta || 0) * (1 - WEIGHT);
-			weighted.theta = weighted.theta * WEIGHT + (w_theta || 0) * (1 - WEIGHT);
-			weighted.engagement = weighted.engagement * WEIGHT + (engagement || 0) * (1 - WEIGHT);
-		}
+	// 	if (weighted.alpha < 0) {
+	// 		weighted.alpha = w_alpha || 0;
+	// 		weighted.beta = w_beta || 0;
+	// 		weighted.theta = w_theta || 0;
+	// 		weighted.engagement = engagement || 0;
+	// 	} else {
+	// 		weighted.alpha = weighted.alpha * WEIGHT + (w_alpha || 0) * (1 - WEIGHT);
+	// 		weighted.beta = weighted.beta * WEIGHT + (w_beta || 0) * (1 - WEIGHT);
+	// 		weighted.theta = weighted.theta * WEIGHT + (w_theta || 0) * (1 - WEIGHT);
+	// 		weighted.engagement = weighted.engagement * WEIGHT + (engagement || 0) * (1 - WEIGHT);
+	// 	}
 
-		if (window.gameInstance.__ready == true) {
-			window.gameInstance.SendMessage("Drone", "SetSpeed", weighted.engagement);
-			dataManager.engagement = weighted.engagement;
+	// 	if (window.gameInstance.__ready == true) {
+	// 		window.gameInstance.SendMessage("Drone", "SetSpeed", weighted.engagement);
+	// 		dataManager.engagement = weighted.engagement;
 
-			document.getElementById("pre-start").style.display = "none";
-			document.getElementById("footer").style.display = "flex";
+	// 		document.getElementById("pre-start").style.display = "none";
+	// 		document.getElementById("footer").style.display = "flex";
+	// 	}
+	// });
+
+	window.Device = new BCIDevice({
+		dataHandler: data => {
+			data.data.forEach(el => {
+				if (buffer.length > BUFFER_SIZE) buffer.shift();
+				buffer.push(el);
+			});
+
+			if (buffer.length < BUFFER_SIZE) return;
+
+			let psd = window.bci.signal.getPSD(BUFFER_SIZE, buffer);
+
+			let alpha = window.bci.signal.getBandPower(BUFFER_SIZE, psd, 256, "alpha");
+			let beta  = window.bci.signal.getBandPower(BUFFER_SIZE,psd, 256, "beta");
+			let theta = window.bci.signal.getBandPower(BUFFER_SIZE,psd, 256, "theta");
+			let gamma = window.bci.signal.getBandPower(BUFFER_SIZE,psd, 256, "gamma");
+			let engagement = beta / (alpha + theta);
+			let sum = alpha + beta + theta + gamma;
+
+			let w_alpha = alpha / sum;
+			let w_beta = beta / sum;
+			let w_theta = theta / sum;
+			let w_gamma = gamma / sum;
+
+			if (weighted.alpha < 0) {
+				weighted.alpha = w_alpha || 0;
+				weighted.beta = w_beta || 0;
+				weighted.theta = w_theta || 0;
+				weighted.gamma = w_gamma || 0;
+				weighted.engagement = engagement || 0;
+			} else {
+				weighted.alpha = weighted.alpha * WEIGHT + (w_alpha || 0) * (1 - WEIGHT);
+				weighted.beta =  weighted.beta  * WEIGHT + (w_beta  || 0)  * (1 - WEIGHT);
+				weighted.theta = weighted.theta * WEIGHT + (w_theta || 0) * (1 - WEIGHT);
+				weighted.gamma = weighted.gamma * WEIGHT + (w_gamma || 0) * (1 - WEIGHT);
+				weighted.engagement = weighted.engagement * WEIGHT + (engagement || 0) * (1 - WEIGHT);
+			}
+
+			if (window.gameInstance.__ready == true) {
+				window.gameInstance.SendMessage("Drone", "SetSpeed", weighted.engagement);
+				dataManager.engagement = weighted.engagement;
+
+				document.getElementById("pre-start").style.display = "none";
+				document.getElementById("footer").style.display = "flex";
+			}
+		},
+		statusHandler: status => {
+			console.log(status)
+		},
+		connectionHandler: connected => {
+			console.log(connected)
 		}
 	});
 
